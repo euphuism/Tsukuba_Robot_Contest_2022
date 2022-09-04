91b844d57de9c69e7e0c7e3637be7d9b33a43219
euphuism <euphuism.s@icloud.com>
2022-09-04 10:18:04 +0900

    commit

--- catkin_ws/src/moter_test/src/moter_control_test_2/moter_control_test_2.ino
+++ catkin_ws/src/moter_test/src/moter_control_test_2/moter_control_test_2.ino
@@ -1,5 +1,5 @@
 #include <ros.h>
-#include <std_msgs/Int64>
+#include <std_msgs/Int64.h>
 
 #define PIN_PWM_LEFT 10
 #define PIN_DIR_LEFT 11
@@ -7,14 +7,14 @@
 #define PIN_DIR_RIGHT 9
 
 ros::NodeHandle nh;
-int64_t pwm1;
+int64_t pwm;
 
-void messangeCb(const std_msgs/Int64& toggle_msg)
+void messangeCb(const std_msgs::Int64& toggle_msg)
 {
-  pwm1 = toggle_msg;
+  pwm = toggle_msg.data;
 }
 
-ros::Subscriber<std_msgs/Int64&> sub("toggle_led", &messangeCb);
+ros::Subscriber<std_msgs::Int64&> sub("toggle_led", &messangeCb);
 
 void setup()
 {
@@ -31,10 +31,10 @@ void loop()
 {
   nh.spinOnce();
 
-  analogWrite(PIN_PWM_LEFT, pwm1);
+  analogWrite(PIN_PWM_LEFT, pwm);
   digitalWrite(PIN_DIR_LEFT, HIGH);
 
-  analogWrite(PIN_PWM_RIGHT, pwm1);
+  analogWrite(PIN_PWM_RIGHT, pwm);
   digitalWrite(PIN_DIR_RIGHT, LOW);
 
   delay(1);
