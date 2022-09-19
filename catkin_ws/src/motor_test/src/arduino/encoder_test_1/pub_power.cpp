311911f98e44a5f4e848a4c4acc3c7647554f38a
euphuism <109351304+euphuism@users.noreply.github.com>
2022-09-20 03:59:16 +0900

    add launch and pub

---
+++ catkin_ws/src/motor_test/src/arduino/encoder_test_1/pub_power.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <msgs/Motor.h>
+
+msgs::Motor control;
+float mot_left = 0.0, mot_right = 0.0;
+
+int main(int argv,char** argc){
+
+  ros::init(argv,argc,"pub_power");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+  ros::Publisher chatter_pub = nh.advertise<msgs::Motor>("chatter", 10);
+  std::String msg_chatter = "HalloWorld";
+  pnh.getParam("power", msg_chatter);
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+    std::stringstream ss;
+    ss << msg_chatter;
+    ss >> mot_left;
+    ss.ignore();
+    ss >> mot_right;
+
+    control.left = mot_left;
+    control.right = mot_right;
+
+    ROS_INFO("LEFT: %s", control.left);
+    ROS_INFO("RIGHT: %s", control.right);
+
+    chatter_pub.publish(control);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
