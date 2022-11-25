8cc24017318b976d4b623955a3f68e263d6cbeb2
Ephuism <109351304+euphuism@users.noreply.github.com>
2022-11-25 22:46:29 +0900

    Update detect_ball_node.cpp

--- catkin_ws/src/detect_ball/src/detect_ball_node.cpp
+++ catkin_ws/src/detect_ball/src/detect_ball_node.cpp
@@ -7,27 +7,26 @@
 
 class DetectBallNode
 {
-private:
+public:
 	ros::Subscriber sub_scan_;
   ros::Publisher pub_cdn_;
+	int i;
 
-	int x[i];
-	int y[i];
-	double cx = 0.0;   // The x-coordinate of the detected ball.
-	double cy = 0.0;   // The y-coordinate of the detected ball.
-	double r  = 0.0;   // The radius of the detected ball.
-	int sumx  = 0;     // Σx
-	int sumy  = 0;     // Σy
-	int sumx2 = 0;     // Σx^2
-	int sumy2 = 0;     // Σy^2
-	int sumxy = 0;     // Σxy
-	int sumx3_xy2 = 0; // Σ(x^3 + x * y^2)
-	int sumx2y_y3 = 0; // Σ(x^2 * y + y^3)
-	int sumx2_y2  = 0; // Σ(x^2 + y^2)
+	double cx = 0.0;//   The x-coordinate of the detected ball.
+	double cy = 0.0;//   The y-coordinate of the detected ball.
+	double r  = 0.0;//   The radius of the detected ball.
+	int sumx  = 0;//     Σx
+	int sumy  = 0;//     Σy
+	int sumx2 = 0;//     Σx^2
+	int sumy2 = 0;//     Σy^2
+	int sumxy = 0;//     Σxy
+	int sumx3_xy2 = 0;// Σ(x^3 + x * y^2)
+	int sumx2y_y3 = 0;// Σ(x^2 * y + y^3)
+	int sumx2_y2  = 0;// Σ(x^2 + y^2)
 	
 	void cbScan(const sensor_msgs::LaserScan::ConstPtr &msg)
 	{
-		int i = msg->ranges.size() / 2;
+		i = msg->ranges.size() / 2;
 		if(msg->ranges[i] < msg->range_min ||
 			 msg->ranges[i] > msg->range_max ||
 			 std::isnan(msg->ranges[i]))
@@ -38,65 +37,67 @@ private:
 		{
 			ROS_INFO("front-range: %0.3f", msg->ranges[msg->ranges.size() / 2]);
 		}
+
+		// https://imagingsolution.blog.fc2.com/blog-entry-16.html
+		
+		int x[i];
+		int y[i];
+		for(int j = 0; j < 2 * i; j++)
+		{
+			x[j] = msg->ranges[2 * j];
+			y[j] = msg->ranges[2 * j + 1];
+			
+			sumx += x[j];
+			sumy += y[j];
+			sumx2 += x[j] * x[j];
+			sumy2 += y[j] * y[j];
+			sumxy += x[j] * y[j];
+			sumx3_xy2 += x[j] * x[j] * x[j] + x[j] * y[j] *y[j];
+			sumx2y_y3 += x[j] * x[j] * y[j] + y[j] * y[j] *y[j];
+			sumx2_y2  += x[j] * x[j] + y[j] * y[j];
+		}
+		/*
+		int F[3][3] = {{sumx2, sumxy, sumx}, 
+									 {sumxy, sumy2, sumy}, 
+									 {sumx, sumy, i}};
+		int G[3][1] = {{-sumx3_xy2}, 
+									 {-sumx2y_y3}, 
+									 {-sumx2_y2}};
+		*/
+		int detF = sumx2 * sumy2 * i + 2 * sumx * sumxy * sumy - sumx * sumx * sumy2 - sumx2 * sumy * sumy - sumxy * sumxy * i;
+		
+    // T = F^(-1) * G
+		int T[3] = 
+			{((sumy2 * i     - sumy  * sumy) * sumx3_xy2 + (sumxy * i    - sumx  * sumy ) * sumx2y_y3 + (sumx  * sumy  - sumxy * sumy ) * sumx2_y2) / detF, 
+			 ((sumxy * i     - sumx  * sumy) * sumx3_xy2 + (sumx  * sumx - sumx2 * i    ) * sumx2y_y3 + (sumx2 * sumy  - sumx  * sumxy) * sumx2_y2) / detF, 
+			 ((sumx  * sumy2 - sumxy * sumy) * sumx3_xy2 + (sumx2 * sumy - sumx  * sumxy) * sumx2y_y3 + (sumxy * sumxy - sumx2 * sumy2) * sumx2_y2) / detF};
+		
+		cx = double(T[0] / (-2));
+		cy = double(T[1] / (-2));
+		r  = std::sqrt(cx * cx + cy * cy - T[2]);
 	}
 
-public:
   DetectBallNode()
   {
     ros::NodeHandle nh;
-    pub_cdn_ = nh.advertise<geometry_msgs::Twist>("cmd_vel", 5);　//chcange message type
-		sub_scan_ = nh.subscribe("scan", 5, &RSJRobotTestNode::cbScan, this);
+		sub_scan_ = nh.subscribe("scan", 10, &DetectBallNode::cbScan, this);
+    pub_cdn_ = nh.advertise<msgs::BallCDN>("ball_cdn", 10);
   }
   void mainloop()
   {
-    ROS_INFO("DetectBallNode");
+    //ROS_INFO("DetectBallNode");
 
     ros::Rate rate(10.0);
     while (ros::ok())
     {
       ros::spinOnce();
 			
-			// https://imagingsolution.blog.fc2.com/blog-entry-16.html
-			for(int j = 0; j < 2 * i; j++)
-			{
-				x[j] = msg->ranges[2 * j];
-				y[j] = msg->ranges[2 * j + 1];
-				
-				sumx += x[j];
-				sumy += y[j];
-				sumx2 += x[j] * x[j];
-				sumy2 += y[j] * y[j];
-				sumxy += x[j] * y[j];
-				sumx3_xy2 += x[j] * x[j] * x[j] + x[j] * y[j] *y[j];
-				sumx2y_y3 += x[j] * x[j] * y[j] + y[j] * y[j] *y[j];
-				sumx2_y2  += x[j] * x[j] + y[j] * y[j];
-			}
-			/*
-			int F[3][3] = {{sumx2, sumxy, sumx}, 
-										 {sumxy, sumy2, sumy}, 
-										 {sumx, sumy, i}};
-			int G[3][1] = {{-sumx3_xy2}, 
-										 {-sumx2y_y3}, 
-										 {-sumx2_y2}};
-			*/
-			int detF = sumx2 * sumy2 * i + 2 * sumx * sumxy * sumy - sumx * sumx * sumy2 - sumx2 * sumy * sumy - sumxy * sumxy * i;
-			
-	    // T = F^(-1) * G
-			int T[3] = 
-				{((sumy2 * i     - sumy  * sumy) * sumx3_xy2 + (sumxy * i    - sumx  * sumy ) * sumx2y_y3 + (sumx  * sumy  - sumxy * sumy ) * sumx2_y2) / detF, 
-				 ((sumxy * i     - sumx  * sumy) * sumx3_xy2 + (sumx  * sumx - sumx2 * i    ) * sumx2y_y3 + (sumx2 * sumy  - sumx  * sumxy) * sumx2_y2) / detF, 
-				 ((sumx  * sumy2 - sumxy * xumy) * sumx3_xy2 + (sumx2 * sumy - sumx  * sumxy) * sumx2y_y3 + (sumxy * sumxy - sumx2 * sumy2) * sumx2_y2) / detF};
-			
-			cx = double(T[0] / (-2));
-			cy = double(T[1] / (-2));
-			r  = std::sqrt(cx * cx + cy * cy - T[2]);
-			
-			hoge_msgs::BallCDN ball_cdn;
+			msgs::BallCDN ball_cdn;
 			ball_cdn.cx_ = cx;
 			ball_cdn.cy_ = cy;
 			ball_cdn.r_  = r;
 			
-			pub_cdn.publish(ball_cdn);
+			pub_cdn_.publish(ball_cdn);
 	
       rate.sleep();
     }
